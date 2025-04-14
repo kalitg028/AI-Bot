881fd4a5a764e4b0cdf32d705e7a2fbe3aff06b7
Techify Bots <TechifyBots@gmail.com>
2025-04-14 13:50:18 +0530

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,40 @@
+class text(object):
+  START = """<b>{},
+
+Welcome to AI Bot – Your Smart Virtual Assistant!
+
+I'm here to assist you with anything you need – from answering questions to solving tasks.
+
+Tap ‘Help’ below to explore all the features!
+
+<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/TechifyRahul'>ʀᴀʜᴜʟ</a></blockquote></b>"""
+
+
+  FORCE_SUBSCRIBE = """<b>{},
+
+To use the bot, you must join our channel first. The bot will not process any requests without joining.
+
+बॉट का उपयोग करने के लिए आपको पहले हमारे चैनल में Join होना होगा। बॉट बिना शामिल हुए किसी भी Request को Process नहीं करेगा।"""
+
+  LOG = """#New_User
+
+User: {}
+ID: {}"""
+  
+  ABOUT = """<b>📜 Cʜᴇᴄᴋ Aʙᴏᴜᴛ:
+  
+Lɪʙʀᴀʀʏ: Pʏʀᴏɢʀᴀᴍ 📚
+Lᴀɴɢᴜᴀɢᴇ: Pʏᴛʜᴏɴ 🐍
+Sᴇʀᴠᴇʀ: Rᴇɴᴅᴇʀ 🌐
+Bᴜɪʟᴅ Sᴛᴀᴛᴜs: V1.3 🚀
+Sᴏᴜʀᴄᴇ Cᴏᴅᴇ: Aᴠᴀɪʟᴀʙʟᴇ (Fʀᴇᴇ) 💻 
+
+<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/TechifyRahul'>ʀᴀʜᴜʟ</a></blockquote></b>"""
+  
+  HELP = """<b>✨ --ᴜsᴇs ᴏꜰ ᴄᴏᴍᴍᴀɴᴅs--
+
+• /ask - ɪꜰ ʏᴏᴜ ᴀʀᴇ ᴜsɪɴɢ ᴛʜɪs ʙᴏᴛ ɪɴ ɢʀᴏᴜᴘ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ᴀsᴋ ᴀɴʏᴛʜɪɴɢ
+
+ᴇx: `/ask what is AI?`
+
+ɴᴏᴛᴇ : ɪɴ ᴘʀɪᴠᴀᴛᴇ ʏᴏᴜ ᴅᴏɴ'ᴛ ɴᴇᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs</b>"""
