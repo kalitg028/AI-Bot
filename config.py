02223251e52a09b17f6fc6db972a42572c0b7754
Techify Bots <TechifyBots@gmail.com>
2025-04-29 10:46:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ""))
 DB_URI = os.getenv("DB_URI", "")
 DB_NAME = os.getenv("DB_NAME", "")
 
-IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100xxxxxxxxx -100xxxxxxx").split())) # Add Multiple channel id
+IS_FSUB = bool(os.getenv("FSUB", True)) # Set "True" For Enable Force Subscribe
+AUTH_CHANNELS = list(map(int, os.getenv("AUTH_CHANNEL", "-100xxxxxxxxx -100xxxxxxx").split())) # Add Multiple channel id
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "")
